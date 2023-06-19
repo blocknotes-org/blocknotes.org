4023c5daa6fd71819f4b838dd411dfb6e76497ff
Ella van Durpe <ella@vandurpe.com>
2023-06-19 20:38:12 +0300

    Fix port

--- assets/index.e045b2e4.js
+++ assets/index.e045b2e4.js
@@ -2938,7 +2938,7 @@ var Encoding;
   Encoding2["UTF16"] = "utf16";
 })(Encoding || (Encoding = {}));
 const Filesystem = registerPlugin("Filesystem", {
-  web: () => __vitePreload(() => import("./web.bcf8383b.js"), true ? [] : void 0).then((m2) => new m2.FilesystemWeb())
+  web: () => __vitePreload(() => import("./web.e467e20e.js"), true ? [] : void 0).then((m2) => new m2.FilesystemWeb())
 });
 var Style;
 (function(Style2) {
@@ -3226,12 +3226,12 @@ var insert = "<?php\n\ninclude 'wordpress/wp-load.php';\n\nfunction insert_items
 StatusBar.setStyle({ style: Style.Dark });
 const platform = Capacitor.getPlatform();
 const url = new URL(window.location);
-let port = "3000";
+let port = "";
 let protocol = "http:";
 if (url.protocol === "https:") {
   protocol = "https:";
 }
-if (url.hostname === "localhost" && url.port) {
+if (url.hostname === "localhost" && !port) {
   port = url.port;
 }
 async function load() {
@@ -3318,7 +3318,7 @@ async function load() {
   try {
     const client = await ir({
       iframe: wp,
-      remoteUrl: `${protocol}//${url.hostname}:${port}/remote.html`,
+      remoteUrl: `${protocol}//${url.hostname}${port ? ":" : ""}${port}/remote.html`,
       blueprint: {
         landingPage: "/wp-admin/edit.php?post_type=hypernote",
         preferredVersions: {
